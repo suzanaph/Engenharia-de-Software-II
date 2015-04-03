a2269051926af7cc140a905cccb214bd93c614e1
netstudenton <manuelmbda@gmail.com>
2015-04-03 18:45:35 -0300

    Class

--- core/src/com/mygdx/dado/Usuario.java
+++ core/src/com/mygdx/dado/Usuario.java
@@ -10,5 +10,28 @@ package com.mygdx.dado;
  * @author manue_000
  */
 public class Usuario {
-    
+   
+    private String nome;
+    private  double pontuacao;
+
+    public Usuario(String nome, double pontuacao) {
+        this.nome = nome;
+        this.pontuacao = pontuacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(double pontuacao) {
+        this.pontuacao = pontuacao;
+    }
 }
