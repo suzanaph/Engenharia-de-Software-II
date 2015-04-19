f34e62c8d8cfa8d50cb63a756055b6a728047da7
netstudenton <manuelmbda@gmail.com>
2015-04-19 14:24:37 -0300

    Regras

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,15 +4,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.jogo.Jogo;
 
-
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		 config.width = 800;
-                config.height = 600;
-                new LwjglApplication(new Jogo(), config);
-                
-            
-                
-	}
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 600;
+        new LwjglApplication(new Jogo(), config);
+
+    }
 }
