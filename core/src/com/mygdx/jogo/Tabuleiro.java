2da06f71189e07b40c87b25667c5fd858f986526
zideon <fabioberlim@id.uff.br>
2015-04-19 11:54:07 -0300

    tabuleiro bonito

--- core/src/com/mygdx/jogo/Tabuleiro.java
+++ core/src/com/mygdx/jogo/Tabuleiro.java
@@ -5,10 +5,30 @@
  */
 package com.mygdx.jogo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author manue_000
  */
 public class Tabuleiro {
+    private static Tabuleiro instancia;
+    int largura=8;
+    int altura=8;
+    int posX;
+    int posY;
+    Area[][] posicoes;
+    private Tabuleiro(){
+        posicoes = new Area[altura][largura];
+        for(int lin=0;lin<altura;lin++)
+            for(int col=0;col<largura;col++)
+               posicoes[lin][col] = new Area(posX + Area.LARGURA* col , posY + Area.ALTURA*lin);
+    }
+    public static Tabuleiro retornaInstancia(){
+        if(instancia==null)
+            instancia = new Tabuleiro();
+        return instancia;
+    }
     
 }
